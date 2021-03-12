94140efc6f28e074757703b0a6191f1151fcfd64
MelonSounds <69148613+MelonSounds@users.noreply.github.com>
2021-03-11 22:55:46 -0600

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,5 +1,16 @@
 var $scene = document.getElementById('game');
 
 function Game() {
+    this.viewport = document.createElement('canvas');
+    this.context = viewport.getContext('2d');
+
+    this.viewport.width = 800;
+    this.viewport.height = 600;
+
+    $scene.insertBefore(this.viewport, $scene.firstChild);
   
+    this.context.font = '32px Arial';
+    this.context.fillText('Welcome!.', 5, 50, 800);
+
+    return this;
 }
