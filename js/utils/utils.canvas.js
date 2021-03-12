3641ee0e12f12c4122ba610a372b1c8808a97e63
MelonSounds <69148613+MelonSounds@users.noreply.github.com>
2021-03-11 22:58:04 -0600

    Update utils.canvas.js

--- js/utils/utils.canvas.js
+++ js/utils/utils.canvas.js
@@ -2,3 +2,28 @@ module.exports = {
     methodOne: function () {},
     methodTwo: function () {}
 };
+
+getPixelRatio : function getPixelRatio(context) {
+  console.log('Determining pixel ratio.');
+
+  // I'd rather not have a giant var declaration block,
+  // so I'm storing the props in an array to dynamically
+  // get the backing ratio.
+  var backingStores = [
+    'webkitBackingStorePixelRatio',
+    'mozBackingStorePixelRatio',
+    'msBackingStorePixelRatio',
+    'oBackingStorePixelRatio',
+    'backingStorePixelRatio'
+  ];
+
+  var deviceRatio = window.devicePixelRatio;
+
+  // Iterate through our backing store props and determine the proper backing ratio.
+  var backingRatio = backingStores.reduce(function(prev, curr) {
+    return (context.hasOwnProperty(curr) ? context[curr] : 1);
+  });
+
+  // Return the proper pixel ratio by dividing the device ratio by the backing ratio
+  return deviceRatio / backingRatio;
+},
