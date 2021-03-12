e4311bf76830711049e1a72be9c50d9f09c95d72
MelonSounds <69148613+MelonSounds@users.noreply.github.com>
2021-03-12 11:11:06 -0600

    Update cheatpad.js

--- cheatpad.js
+++ cheatpad.js
@@ -1,7 +1,13 @@
 // Cheatpad, from PadSafe and Aim Entertainment
 
+var special = true
+
 function cheatPad() {
-  if (2D = true) {
+  if (special = true) {
+    
     // This will make special commands for the 2D anticheat!
+  } else {
+    
+    // This means the game is 3D and has special elements!
   }
 }
