6be40443d9afaca5f2bd529d12952b1ca97963ba
MelonSounds <69148613+MelonSounds@users.noreply.github.com>
2021-03-11 22:58:46 -0600

    Update utils.canvas.js

--- js/utils/utils.canvas.js
+++ js/utils/utils.canvas.js
@@ -46,3 +46,30 @@ generateCanvas : function generateCanvas(w, h) {
 
   return canvas;
 }
+
+var cUtils = require('./utils/utils.canvas.js'),
+    $container = document.getElementById('container');
+
+function Game(w, h) {
+  // Generate a canvas and store it as our viewport
+  this.viewport = cUtils.generateCanvas(w, h);
+  this.viewport.id = "gameViewport"; // give the canvas an ID for easy CSS/JS targeting
+
+  // Get and store the canvas context as a global
+  this.context = this.viewport.getContext('2d');
+
+  // Append our viewport into a container in the dom
+  $container.insertBefore(this.viewport, $container.firstChild);
+
+  // Spit out some text
+  this.context.font = '32px Arial';
+  this.context.fillStyle = '#fff';
+  this.context.fillText('It\'s dangerous to travel this route alone.', 5, 50);
+
+  return this;
+}
+
+// Instantiate a new game in the global scope at 800px by 600px
+window.game = new Game(800, 600);
+
+module.exports = game;
